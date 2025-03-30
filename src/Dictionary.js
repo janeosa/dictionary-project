2b1ff97dfbb52beceb2f77a07653cda8452be63f
miss-jane-osa <jane_i@hotmail.co.uk>
2025-03-30 22:01:00 +0100

    Added Axios and Dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    let apiKey = "10tc88b5de8d7eoa39c8f2ea84faadb2";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
